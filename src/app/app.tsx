61cc20cbfd2db812c125c22a6675a9b92c05fe09
SergeiU <heartofkerrigan@gmail.com>
2024-07-12 00:24:46 +0900

    создает компонент app

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import MainPage from '../components/pages/main-page/main-page';
+
+type AppScreenProps = {
+  cardsCount: number;
+};
+
+function App({ cardsCount }: AppScreenProps): JSX.Element {
+  return <MainPage cardsCount={cardsCount} />;
+}
+
+export default App;
