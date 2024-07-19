8bd18fda6bf729b8b85c189fd25d1596beff5ba9
SergeiU <heartofkerrigan@gmail.com>
2024-07-19 22:34:27 +0900

    разбивает на компоненты

--- src/components/pages/favorites-page/favorites-empty-page.tsx
+++ src/components/pages/favorites-page/favorites-empty-page.tsx
@@ -1,39 +1,22 @@
-function FavoritesEmptyList(): JSX.Element {
+import Logo from '../../header/logo/logo';
+import FavoritesCount from '../../header/user/favorites-count';
+import SignMenu from '../../header/user/sign-menu';
+import Username from '../../header/user/username';
+
+function FavoritesEmptyPage(): JSX.Element {
   return (
     <div className="page page--favorites-empty">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">0</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
+                <Username />
+                <FavoritesCount favoriteCount={0} />
+                <SignMenu />
               </ul>
             </nav>
           </div>
@@ -54,18 +37,10 @@ function FavoritesEmptyList(): JSX.Element {
         </div>
       </main>
       <footer className="footer">
-        <a className="footer__logo-link" href="main.html">
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width="64"
-            height="33"
-          />
-        </a>
+        <Logo />
       </footer>
     </div>
   );
 }
 
-export default FavoritesEmptyList;
+export default FavoritesEmptyPage;
--- src/components/pages/favorites-page/favorites-page.tsx
+++ src/components/pages/favorites-page/favorites-page.tsx
@@ -2,43 +2,25 @@ import {
   FavoritesCardAmsterdam,
   FavoritesCardCologne,
 } from '../../cards/favorites-cards';
+import Logo from '../../header/logo/logo';
+import Username from '../../header/user/username';
+import SignMenu from '../../header/user/sign-menu';
+import FavoritesCount from '../../header/user/favorites-count';
 
-function FavoritesList(): JSX.Element {
+function FavoritesPage(): JSX.Element {
   return (
     <div className="page">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
+                <Username />
+                <FavoritesCount favoriteCount={3} />
+                <SignMenu />
               </ul>
             </nav>
           </div>
@@ -80,18 +62,10 @@ function FavoritesList(): JSX.Element {
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width="64"
-            height="33"
-          />
-        </a>
+        <Logo />
       </footer>
     </div>
   );
 }
 
-export default FavoritesList;
+export default FavoritesPage;
